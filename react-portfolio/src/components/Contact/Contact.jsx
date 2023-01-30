88453508b7e0e6485d0ac9c8527bfb426bdc7200
Sabrina Nawaz <sabrinanawaz23@gmail.com>
2023-01-29 20:16:16 -0500

    Edited Contact CSS file and created About Me component

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -3,19 +3,19 @@ import './Contact.css'
 
 function Contact() {
     return (
-        <section>
-        <h2>Contact me</h2>
-          <h4>Please see my contact information below!</h4>
-            <div className="mt-2">
-              Email:{" "}
-              <a href="mailto:sabrinanawaz23@gmail.com">sabrinanawaz23@gmail.com</a>
-              <br />
-              Github:{" "}
-              <a href="https://github.com/Sabrina-Nawaz">
-                https://github.com/Sabrina-Nawaz
-              </a>
-              <br />
-            </div>
+      <section id="contact" class="side-bar contactme">
+      <h2>Contact Me</h2>
+      <div>
+      <br />
+          <a href="mailto:sabrinanawaz23@gmail.com">Email</a>
+          <br />
+          <a href="tel:555-555-5555">Phone</a>
+          <br />
+          <a href="https://www.linkedin.com/in/nawazs/">LinkedIn</a>
+          <br />
+          <a href="#">Resume</a>
+      </div>
+
       </section>
     );
   }
